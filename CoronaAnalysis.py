710e00885dbcfb2464a4fcc553c34fa814285700
cjhuangster <39141143+cjhuangster@users.noreply.github.com>
2020-04-18 21:52:03 -0700

    fetched data for testing and cases

--- CoronaAnalysis.py
+++ CoronaAnalysis.py
@@ -4,7 +4,7 @@
 # data = pd.read_excel('D:\CJ\Academic\CMPT340\Project\Public_COVID-19_Canada.xlsx',sheet_name='Cases', header = 3, skiprows=3, index_col=0)
 # # data = pd.ExcelFile.parse('D:\CJ\Academic\CMPT340\Project\Public_COVID-19_Canada.xlsx',sheet_name='Cases', header = 3, skiprows=3, index_col=0)
 # # print(data)
-# print(data[['age']])
+# print(data[['ageCase']])
 
 from xlrd import *
 file = open_workbook('D:\CJ\Academic\CMPT340\Project\Public_COVID-19_Canada.xlsx')
@@ -15,68 +15,77 @@ for i in range(0,4):
     data.append(file.sheet_by_index(i)) 
 
 # rows and colums 0 indexed: row 0-2 = readme, 3 = header, 4 onwards = data
-numCol = data[0].ncols
-numRow = data[0].nrows
-# print(numCol, numRow)
-colData = []
-# get data of 'Cases' by column
-for i in range(0, numCol):
-    col = data[0].col_values(i)
-    colData.append(col)
-# print(colData[0][4])
-rowData = []
-for i in range(0, numRow):
+numColCases = data[0].ncols
+numRowCases = data[0].nrows
+rowCases = []
+for i in range(0, numRowCases):
     row = data[0].row_values(i)
-    rowData.append(row)
+    rowCases.append(row)
 
+numColTesting = data[3].ncols
+numRowTesting = data[3].nrows
+rowTesting = []
+for i in range(0, numRowTesting):
+    row = data[3].row_values(i)
+    rowTesting.append(row)    
 
-# BC = []
-# Ontario = []
-# Manitoba = []
-# Alberta = []
-# Quebec = []
-# Saskatchewan = []
-# PEI = []
-# NL = []
-# NovaScotia = []
+ageCase = []
+sexCase = []
+healthRegionCase = []
+provinceCase = []
+dateReportedCase = []
 
-age = []
-sex = []
-healthRegion = []
-province = []
-dateReported = []
+provinceTesting = []
 
 # initialize data category for each header
-# 0CaseID. 1ProvCase. 2Age. 3Sex. 4HealthRegion. 5Province. 6Country. 7DateReported. 8WeekReported
-for i in range(4, numRow):
-    if rowData[i][2] not in age:
-        age.append(rowData[i][2])
-    if rowData[i][3] not in sex:
-        sex.append(rowData[i][3])
-    if rowData[i][4] not in healthRegion:
-        healthRegion.append(rowData[i][4])
-    if rowData[i][5] not in province:
-        province.append(rowData[i][5])
-    if rowData[i][7] not in dateReported:
-            dateReported.append(rowData[i][7])
+# 0CaseID. 1ProvCase. 2ageCase. 3sexCase. 4healthRegionCase. 5provinceCase. 6Country. 7dateReportedCase. 8WeekReported
+for i in range(4, numRowCases):
+    if rowCases[i][2] not in ageCase:
+        ageCase.append(rowCases[i][2])
+    if rowCases[i][3] not in sexCase:
+        sexCase.append(rowCases[i][3])
+    if rowCases[i][4] not in healthRegionCase:
+        healthRegionCase.append(rowCases[i][4])
+    if rowCases[i][5] not in provinceCase:
+        provinceCase.append(rowCases[i][5])
+    if rowCases[i][7] not in dateReportedCase:
+            dateReportedCase.append(rowCases[i][7])
+            
+for i in range(4,numRowTesting):
+    if rowTesting[i][1] not in provinceTesting:
+        provinceTesting.append(rowTesting[i][1])
 
 # initialize data by each category
-ageData = [[] for i in age]
-sexData = [[] for i in sex]
-healthRegionData = [[] for i in healthRegion]
-provinceData = [[] for i in province]
-dateReportedData = [[] for i in dateReported]    
+ageCaseData = [[] for i in ageCase]
+sexCaseData = [[] for i in sexCase]
+healthRegionCaseData = [[] for i in healthRegionCase]
+provinceCaseData = [[] for i in provinceCase]
+dateReportedCaseData = [[] for i in dateReportedCase]    
+provinceTestingData = [[] for i in provinceTesting]
 
 # categorize data
-# 0CaseID. 1ProvCase. 2Age. 3Sex. 4HealthRegion. 5Province. 6Country. 7DateReported. 8WeekReported
-for i in range(4,numRow):
-    ageData[age.index(rowData[i][2])].append(rowData[i])
-    sexData[sex.index(rowData[i][3])].append(rowData[i])
-    healthRegionData[healthRegion.index(rowData[i][4])].append(rowData[i])
-    provinceData[province.index(rowData[i][5])].append(rowData[i])
-    dateReportedData[dateReported.index(rowData[i][7])].append(rowData[i])
+# 0CaseID. 1ProvCase. 2ageCase. 3sexCase. 4healthRegionCase. 5provinceCase. 6Country. 7dateReportedCase. 8WeekReported
+for i in range(4,numRowCases):
+    ageCaseData[ageCase.index(rowCases[i][2])].append(rowCases[i])
+    sexCaseData[sexCase.index(rowCases[i][3])].append(rowCases[i])
+    healthRegionCaseData[healthRegionCase.index(rowCases[i][4])].append(rowCases[i])
+    provinceCaseData[provinceCase.index(rowCases[i][5])].append(rowCases[i])
+    dateReportedCaseData[dateReportedCase.index(rowCases[i][7])].append(rowCases[i])
+
+for i in range(4,numRowTesting):
+    provinceTestingData[provinceTesting.index(rowTesting[i][1])].append(rowTesting[i])
+    
+    
+# test for checking data
+# for i in healthRegionCaseData:
+#     for j in i:
+#         print(j[4])
 
 # test for checking data
-for i in healthRegionData:
-    for j in i:
-        print(j[4])
+# for i in provinceTestingData:
+#     for j in i:
+#         print(j[1])
+
+
+# have projected data kept in variable to be called by API
+# project rate of infection vs Test to future rate
